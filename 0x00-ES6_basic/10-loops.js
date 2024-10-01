c9a70b0f92b46a9fc5178299caad12d6c8b1cd72
JoyChinaza <nwosuchinaza22@gmail.com>
2024-10-01 01:25:47 +0000

    10 loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx of array.keys()) {
+    let value = array[idx];
+    array[idx] = appendString + value;
+  }
+  return array;
+}
