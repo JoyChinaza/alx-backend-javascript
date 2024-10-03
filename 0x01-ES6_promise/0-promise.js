1360c7f3190b3083a7fff96131b7f739eceb39f0
JoyChinaza <nwosuchinaza22@gmail.com>
2024-10-03 19:09:17 +0000

    0 promis

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,18 +1,4 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulating an API call with setTimeout
-    setTimeout(() => {
-      const success = Math.random() > 0.5; // Randomly succeed or fail
-      if (success) {
-        resolve("API response data");
-      } else {
-        reject(new Error("API request failed"));
-      }
-    }, 1000); // Simulate a 1 second delay
-  });
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
 }
-
-// Usage:
-getResponseFromAPI()
-  .then(response => console.log("Success:", response))
-  .catch(error => console.error("Error:", error));
