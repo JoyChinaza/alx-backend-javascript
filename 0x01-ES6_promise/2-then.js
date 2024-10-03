29c5b55d26d2d82dab5bc3909a0dee720b06c906
JoyChinaza <nwosuchinaza22@gmail.com>
2024-10-03 23:40:20 +0000

    2 then js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,30 +1,6 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      return {
-        status: 200,
-        body: 'success'
-      };
-    })
-    .catch(() => {
-      return new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
-
-// Example usage with a resolved and rejected promise:
-const resolvedPromise = Promise.resolve();
-const rejectedPromise = Promise.reject();
-
-// Testing with resolved promise
-handleResponseFromAPI(resolvedPromise).then((result) => {
-  console.log(result); // { status: 200, body: 'success' }
-});
-
-// Testing with rejected promise
-handleResponseFromAPI(rejectedPromise).then((result) => {
-  console.log(result); // Error {}
-});
-
