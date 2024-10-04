1e08d6b3672fe0d536e76e73a89875b2a9b76318
JoyChinaza <nwosuchinaza22@gmail.com>
2024-10-04 04:15:36 +0000

    9 try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,14 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(`Error: ${error.message}`);
+    queue.push(error.message);
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
-}
 }
